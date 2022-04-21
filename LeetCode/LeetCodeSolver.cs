2bf9bc8f8d8d036a51ca4a5080323653e826268e
IvanGu <gusagusa@gmail.com>
2022-04-21 20:26:00 +0300

    Добавьте файлы проекта.

---
+++ LeetCode/LeetCodeSolver.cs
@@ -0,0 +1,77 @@
+﻿namespace LeetCodeSolver
+{
+
+    public class LeetCodeSolver
+    {
+        //Remove Duplicates from Sorted Array
+        //https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
+        public int RemoveDuplicates(int[] nums)
+        {
+            int[] nums2 = nums.Distinct().ToArray();
+            nums2.CopyTo(nums, 0);
+            return nums2.Length;
+        }
+
+
+        //Rotate Array
+        //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
+        public void Rotate(int[] nums, int k)
+        {
+            if (k < 0) return;
+
+            k %= nums.Length;
+            int[] nums1 = nums[..(nums.Length - k)];
+            int[] nums2 = nums[^k..];
+            nums2.CopyTo(nums, 0);
+            nums1.CopyTo(nums, nums2.Length);
+        }
+
+
+        //Two Sum
+        //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new();
+            for (int i = 0; i < nums.Length; i++)
+                dict[nums[i]] = i;
+            for (int i = 0; i < nums.Length; i++)
+                if (dict.TryGetValue(target - nums[i], out int j))
+                    if (i != j) return new int[] { i, j };
+
+            throw new KeyNotFoundException("not found");
+        }
+
+
+        //Single Number
+        //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+        public int SingleNumber(int[] nums)
+        {
+            var v = nums.GroupBy(x => x)
+                .Select(g => new { Name = g.Key, Count = g.Count() })
+                .Where(a => a.Count == 1)
+                .FirstOrDefault();
+            return v?.Name ?? throw new KeyNotFoundException("not found");
+        }
+
+
+        //Plus One
+        //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/
+        public int[] PlusOne(int[] digits)
+        {
+            int addInt = 1;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                addInt += digits[i];
+                digits[i] = addInt % 10;
+                addInt /= 10;
+            }
+            if (addInt == 0) return digits;
+            int[] res = new int[digits.Length + 1];
+            res[0] = addInt;
+            digits.CopyTo(res, 1);
+            return res;
+        }
+
+    }
+}
---
+++ LeetCodeTests1/LeetCodeSolverTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeSolver;
+
+namespace LeetCodeSolver.Tests
+{
+    [TestClass()]
+    public class LeetCodeSolverTests
+    {
+        [TestMethod()]
+        public void RemoveDuplicatesTest()
+        {
+            int[] nums = { 1, 1, 2, 3, 3, 3, 4 }; // Input array
+            int[] expectedNums = { 1, 2, 3, 4 }; // The expected answer with correct length
+
+            int k = new LeetCodeSolver().RemoveDuplicates(nums); // Calls your implementation
+
+            Assert.AreEqual(k, expectedNums.Length);
+            for (int i = 0; i < k; i++)
+                Assert.AreEqual(nums[i], expectedNums[i]);
+        }
+
+
+        [TestMethod()]
+        public void RotateTest()
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 }; // Input array
+            int[] expectedNums = { 5, 6, 7, 1, 2, 3, 4 }; // The expected answer with correct length
+            int k = 3;
+
+            new LeetCodeSolver().Rotate(nums, k);
+
+            for (int i = 0; i < nums.Length; i++)
+                Assert.AreEqual(nums[i], expectedNums[i]);
+        }
+
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            int[] nums = { 3, 3 }; // Input array
+            int[] expectedNums = { 0, 1 }; // The expected answer with correct length
+            int target = 6;
+            //int[] nums = { 2, 7, 11, 15 }; // Input array
+            //int[] expectedNums = { 0, 1 }; // The expected answer with correct length
+            //int target = 9;
+
+            nums = new LeetCodeSolver().TwoSum(nums, target);
+            for (int i = 0; i < nums.Length; i++)
+                Assert.AreEqual(nums[i], expectedNums[i]);
+        }
+
+        [TestMethod()]
+        public void SingleNumberTest()
+        {
+
+            int[] nums = { 3, 3, 1, 2, 2 }; // Input array
+            int target = 1;
+
+            Assert.AreEqual(new LeetCodeSolver().SingleNumber(nums), target);
+
+        }
+
+        [TestMethod()]
+        public void PlusOneTest()
+        {
+            int[] nums = { 8, 9 }; // Input array
+            int[] expectedNums = {  9, 0 }; // The expected answer with correct length
+            //int[] nums = { 9, 9 }; // Input array
+            //int[] expectedNums = { 1, 0, 0 }; // The expected answer with correct length
+
+            nums = new LeetCodeSolver().PlusOne(nums);
+
+            Assert.AreEqual(nums.Length, expectedNums.Length);
+            for (int i = 0; i < nums.Length; i++)
+                Assert.AreEqual(nums[i], expectedNums[i]);
+
+        }
+    }
+}
